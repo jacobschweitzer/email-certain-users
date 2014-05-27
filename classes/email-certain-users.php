afa5fa25c9c34bab394a345a19fe9ec2693d96a6
Jacob Schweitzer <allambition@gmail.com>
2014-05-27 17:30:43 -0300

    changed main class name changed mentions of wpps_ to ecu_ gutted init function and commented out its action calls

--- classes/email-certain-users.php
+++ classes/email-certain-users.php
@@ -1,19 +1,19 @@
 <?php
 
-if ( ! class_exists( 'WordPress_Plugin_Skeleton' ) ) {
+if ( ! class_exists( 'Email_Certain_Users' ) ) {
 
 	/**
 	 * Main / front controller class
 	 *
-	 * WordPress_Plugin_Skeleton is an object-oriented/MVC base for building WordPress plugins
+	 * Email_Certain_users is an object-oriented/MVC base for building WordPress plugins
 	 */
-	class WordPress_Plugin_Skeleton extends WPPS_Module {
+	class Email_Certain_Users extends ECU_Module {
 		protected static $readable_properties  = array();    // These should really be constants, but PHP doesn't allow class constants to be arrays
 		protected static $writeable_properties = array();
 		protected $modules;
 
-		const VERSION    = '0.4a';
-		const PREFIX     = 'wpps_';
+		const VERSION    = '1.0';
+		const PREFIX     = 'ecu_';
 		const DEBUG_MODE = false;
 
 
@@ -30,9 +30,8 @@ if ( ! class_exists( 'WordPress_Plugin_Skeleton' ) ) {
 			$this->register_hook_callbacks();
 
 			$this->modules = array(
-				'WPPS_Settings'    => WPPS_Settings::get_instance(),
-				'WPPS_CPT_Example' => WPPS_CPT_Example::get_instance(),
-				'WPPS_Cron'        => WPPS_Cron::get_instance()
+				'ECU_Settings'    => ECU_Settings::get_instance(),
+				//'WPPS_Cron'        => WPPS_Cron::get_instance()
 			);
 		}
 
@@ -166,8 +165,8 @@ if ( ! class_exists( 'WordPress_Plugin_Skeleton' ) ) {
 		 */
 		public function register_hook_callbacks() {
 			add_action( 'wpmu_new_blog',         __CLASS__ . '::activate_new_site' );
-			add_action( 'wp_enqueue_scripts',    __CLASS__ . '::load_resources' );
-			add_action( 'admin_enqueue_scripts', __CLASS__ . '::load_resources' );
+			//add_action( 'wp_enqueue_scripts',    __CLASS__ . '::load_resources' );
+			//add_action( 'admin_enqueue_scripts', __CLASS__ . '::load_resources' );
 
 			add_action( 'init',                  array( $this, 'init' ) );
 			add_action( 'init',                  array( $this, 'upgrade' ), 11 );
@@ -179,12 +178,7 @@ if ( ! class_exists( 'WordPress_Plugin_Skeleton' ) ) {
 		 * @mvc Controller
 		 */
 		public function init() {
-			try {
-				$instance_example = new WPPS_Instance_Class( 'Instance example', '42' );
-				//add_notice( $instance_example->foo .' '. $instance_example->bar );
-			} catch ( Exception $exception ) {
-				add_notice( __METHOD__ . ' error: ' . $exception->getMessage(), 'error' );
-			}
+			
 		}
 
 		/**
@@ -195,15 +189,15 @@ if ( ! class_exists( 'WordPress_Plugin_Skeleton' ) ) {
 		 * @param string $db_version
 		 */
 		public function upgrade( $db_version = 0 ) {
-			if ( version_compare( $this->modules['WPPS_Settings']->settings['db-version'], self::VERSION, '==' ) ) {
+			if ( version_compare( $this->modules['ECU_Settings']->settings['db-version'], self::VERSION, '==' ) ) {
 				return;
 			}
 
 			foreach ( $this->modules as $module ) {
-				$module->upgrade( $this->modules['WPPS_Settings']->settings['db-version'] );
+				$module->upgrade( $this->modules['ECU_Settings']->settings['db-version'] );
 			}
 
-			$this->modules['WPPS_Settings']->settings = array( 'db-version' => self::VERSION );
+			$this->modules['ECU_Settings']->settings = array( 'db-version' => self::VERSION );
 			self::clear_caching_plugins();
 		}
 
